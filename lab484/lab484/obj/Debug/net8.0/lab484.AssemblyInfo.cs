d17728d7c21463ec05db0173c890969bda4c778f
Nick <nickaclement22@gmail.com>
2025-02-18 15:17:11 -0500

    test

--- lab484/lab484/obj/Debug/net8.0/lab484.AssemblyInfo.cs
+++ lab484/lab484/obj/Debug/net8.0/lab484.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("lab484")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d3726a5eab384467c4196ae417d5ce780ef6f818")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+66d16c03bab27add3f0850b7fd106184857794d5")]
 [assembly: System.Reflection.AssemblyProductAttribute("lab484")]
 [assembly: System.Reflection.AssemblyTitleAttribute("lab484")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
