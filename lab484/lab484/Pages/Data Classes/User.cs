9475fe9d6c904ca41136ca38c4f322103e09d234
Nick <nickaclement22@gmail.com>
2025-02-18 13:08:52 -0500

    Created User Subclasses

--- lab484/lab484/Pages/Data Classes/User.cs
+++ lab484/lab484/Pages/Data Classes/User.cs
@@ -3,10 +3,35 @@
     public class User
     {
         public int UserID { get; set; }
-
         public String? UserName { get; set; }
         public String? Password { get; set; }
         public String? FirstName { get; set; }
         public String? LastName { get; set; }
+        public String? Email { get; set; }
+        public String? Phone { get; set; }
+        public String? HomeAddress { get; set; }
+    }
+
+    public class Employee : User
+    {
+        public new int UserID { get; set; }
+        public Boolean? AdminStatus { get; set; }
+
+    }
+
+    public class BPrep : User
+    {
+        public new int UserID { get; set; }
+        public String? CommunicationStatus { get; set; }
+    }
+
+    public class Faculty : User
+    {
+        public new int UserID { get; set; }
+    }
+
+    public class NonFaculty : User
+    {
+        public new int UserID { get; set; }
     }
 }
