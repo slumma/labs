5bf8012a4553e97bb2e10ddc2fb2c8dd8cc10e76
Nick <nickaclement22@gmail.com>
2025-02-18 14:27:13 -0500

    BPDetail Added

---
+++ lab484/lab484/Pages/Data Classes/BusinessPartner.cs
@@ -0,0 +1,17 @@
+﻿namespace lab484.Pages.Data_Classes
+{
+    public class BusinessPartner
+    {
+        public int SupplierID { get; set; }
+        public String SupplierName { get; set; }
+        public String OrgType { get; set; }
+        public String? CommunicationStatus { get; set; }
+        public int UserID { get; set; }
+        public String? FirstName { get; set; }
+        public String? LastName { get; set; }
+        public String? Email { get; set; }
+        public String? Phone { get; set; }
+        public String? HomeAddress { get; set; }
+        public String? StatusName { get; set; }
+    }
+}
--- lab484/lab484/obj/Debug/net8.0/lab484.AssemblyInfo.cs
+++ lab484/lab484/obj/Debug/net8.0/lab484.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("lab484")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d75ec39849f2fe736f1cfa2fb0534da6e2dd7576")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d3726a5eab384467c4196ae417d5ce780ef6f818")]
 [assembly: System.Reflection.AssemblyProductAttribute("lab484")]
 [assembly: System.Reflection.AssemblyTitleAttribute("lab484")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
