8dd6ac7e770f1a631a1151e8de5f44e0c5b44f95
Nick <nickaclement22@gmail.com>
2025-02-20 11:54:16 -0500

    addgrant added

---
+++ lab484/lab484/Pages/Faculty/AddGrant.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace lab484.Pages.Faculty
+{
+    public class AddGrantModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
